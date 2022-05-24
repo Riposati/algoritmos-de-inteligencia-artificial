ccfbd61aa36afe82bae247cfa2576fed21f5ffbb
Gustavo Riposati <guriposa@gmail.com>
2022-05-23 22:07:56 -0300

    Delete midiarank directory

--- midiarank/resources/lang/en/loginNew.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-   'EmailAddress' => 'Email',
-   'Password' => 'Password',
-   'RememberMe' => 'Remember me',
-   'ForgotYourPassword' => 'Forgot Your Password?',
-];
--- midiarank/resources/lang/pt/loginNew.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-
-   'EmailAddress' => 'Email',
-   'Password' => 'Senha',
-   'RememberMe' => 'Lembrar senha',
-   'ForgotYourPassword' => 'Esqueceu sua senha?',
-];
